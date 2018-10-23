d39bcfb4fc3193e1bb9b27b235b9af272166c9b0
Ryan Brady <ryanbrady369@gmail.com>
2018-10-23 21:16:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,9 @@
 class Book
-  def initialize(title, author = nil, page_count = nil, genre = nil)
+  def initialize(title)#, author = nil, page_count = nil, genre = nil)
     @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
+    # @author = author
+    # @page_count = page_count
+    # @genre = genre
   end
   attr_accessor :title, :author, :page_count, :genre
 end
